58d875f2a599999541d312b5c59f1b2f56aa8db2
s-kakol <kakolsylwester@gmail.com>
2023-03-17 19:16:50 +0100

    Modify Tipper to calculate BMI

---
+++ BMI_Tipper/README.md
@@ -0,0 +1,1 @@
+![BMI Calculator](img.png)
--- BMI_Tipper/app/src/main/java/com/example/tipper/MainActivity.java
+++ BMI_Tipper/app/src/main/java/com/example/tipper/MainActivity.java
@@ -5,27 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable; // for EditText event handling
 import android.text.TextWatcher; // EditText listener
-import android.widget.EditText; // for bill amount input
-import android.widget.SeekBar; // for changing the tip percentage
-import android.widget.SeekBar.OnSeekBarChangeListener; // SeekBar listener
+import android.widget.EditText;
 import android.widget.TextView; // for displaying text
 
 import java.text.NumberFormat; // for currency formatting
 
 public class MainActivity extends AppCompatActivity {
-
-    // currency and percent formatter objects
-    private static final NumberFormat currencyFormat =
-            NumberFormat.getCurrencyInstance();
-    private static final NumberFormat percentFormat =
-            NumberFormat.getPercentInstance();
-
-    private double billAmount = 0.0; // bill amount entered by the user
-    private double percent = 0.15; // initial tip percentage
-    private TextView amountTextView; // shows formatted bill amount
-    private TextView percentTextView; // shows tip percentage
-    private TextView tipTextView; // shows calculated tip amount
-    private TextView totalTextView; // shows calculated total bill amount
+    private int cmHeight = 0; // height entered by the user
+    private double kgWeight = 0.0; // weight entered by the user
+    private TextView heightTextView; // shows the height
+    private TextView weightTextView; // shows the weight
+    private TextView totalTextView; // shows calculated bmi
 
     // called when the activity is first created
     @Override
@@ -34,81 +24,80 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main); // inflate the GUI
 
         // get references to programmatically manipulated TextViews
-        amountTextView = (TextView) findViewById(R.id.amountTextView);
-        percentTextView = (TextView) findViewById(R.id.percentTextView);
-        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        heightTextView = (TextView) findViewById(R.id.heightTextView);
+        weightTextView = (TextView) findViewById(R.id.weightTextView);
         totalTextView = (TextView) findViewById(R.id.totalTextView);
-        tipTextView.setText(currencyFormat.format(0));
-        totalTextView.setText(currencyFormat.format(0));
+        totalTextView.setText(String.format(0 + ""));
 
-        // set amountEditText's TextWatcher
-        EditText amountEditText =
-                (EditText) findViewById(R.id.amountEditText);
-        amountEditText.addTextChangedListener(amountEditTextWatcher);
+        // set watchers
+        EditText heightEditText =
+                (EditText) findViewById(R.id.heightEditText);
+        heightEditText.addTextChangedListener(heightEditTextWatcher);
 
-        // set percentSeekBar's OnSeekBarChangeListener
-        SeekBar percentSeekBar =
-                (SeekBar) findViewById(R.id.percentSeekBar);
-        percentSeekBar.setOnSeekBarChangeListener(seekBarListener);
+        EditText weightEditText =
+                (EditText) findViewById(R.id.weightEditText);
+        weightEditText.addTextChangedListener(weightEditTextWatcher);
     }
 
     // calculate and display tip and total amounts
     private void calculate() {
-        // format percent and display in percentTextView
-        percentTextView.setText(percentFormat.format(percent));
-
-        // calculate the tip and total
-        double tip = billAmount * percent;
-        double total = billAmount + tip;
-
-        // display tip and total formatted as currency
-        tipTextView.setText(currencyFormat.format(tip));
-        totalTextView.setText(currencyFormat.format(total));
+        double bmi = (kgWeight / cmHeight / cmHeight) * 10000;
+        totalTextView.setText(String.format((Math.round(bmi * 100.0) / 100.0) + ""));
     }
 
-    // listener object for the SeekBar's progress changed events
-    private final OnSeekBarChangeListener seekBarListener =
-            new OnSeekBarChangeListener() {
-                // update percent, then call calculate
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress,
-                                              boolean fromUser) {
-                    percent = progress / 100.0; // set percent based on progress
-                    calculate(); // calculate and display tip and total
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) { }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) { }
-            };
-
     // listener object for the EditText's text-changed events
-    private final TextWatcher amountEditTextWatcher = new TextWatcher() {
-        // called when the user modifies the bill amount
+    private final TextWatcher heightEditTextWatcher = new TextWatcher() {
+        // called when the user modifies the height
         @Override
         public void onTextChanged(CharSequence s, int start,
                                   int before, int count) {
 
-            try { // get bill amount and display currency formatted value
-                billAmount = Double.parseDouble(s.toString()) / 100.0;
-                amountTextView.setText(currencyFormat.format(billAmount));
-            }
-            catch (NumberFormatException e) { // if s is empty or non-numeric
-                amountTextView.setText("");
-                billAmount = 0.0;
+            try {
+                cmHeight = Integer.parseInt(s.toString());
+                heightTextView.setText(String.format(cmHeight + ""));
+            } catch (NumberFormatException e) { // if s is empty or non-numeric
+                heightTextView.setText("");
+                cmHeight = before;
             }
 
-            calculate(); // update the tip and total TextViews
+            calculate(); // update bmi value
         }
 
         @Override
-        public void afterTextChanged(Editable s) { }
+        public void afterTextChanged(Editable s) {
+        }
 
         @Override
         public void beforeTextChanged(
-                CharSequence s, int start, int count, int after) { }
+                CharSequence s, int start, int count, int after) {
+        }
+    };
+
+    private final TextWatcher weightEditTextWatcher = new TextWatcher() {
+        // called when the user modifies the weight
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try {
+                kgWeight = Double.parseDouble(s.toString());
+                weightTextView.setText(String.format(kgWeight + ""));
+            } catch (NumberFormatException e) { // if s is empty or non-numeric
+                weightTextView.setText("");
+                kgWeight = before * 1.0;
+            }
+
+            calculate(); // update bmi value
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) {
+        }
     };
 }
 
